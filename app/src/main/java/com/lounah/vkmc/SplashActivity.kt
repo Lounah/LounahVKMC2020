81d6a11d77589dca16c32fe27254d5bf04c6e29e
lounah <lounahxrussia@gmail.com>
2020-02-14 21:08:03 +0300

    VK Sdk setup

---
+++ app/src/main/java/com/lounah/vkmc/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.lounah.vkmc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+internal class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
