8cf8d39d7f7b579b6c7286941881eec1194d5667
m.shchepalin <m.shchepalin@tinkoff.ru>
2020-02-20 00:22:34 +0300

    ViewHolder setup

--- feature/feature-market/src/main/java/com/lounah/vkmc/feature/feature_market/goods/ui/recycler/ProductUi.kt
+++ feature/feature-market/src/main/java/com/lounah/vkmc/feature/feature_market/goods/ui/recycler/ProductUi.kt
@@ -1,14 +1,19 @@
 package com.lounah.vkmc.feature.feature_market.goods.ui.recycler
 
 import android.view.View
+import com.bumptech.glide.request.RequestOptions
 import com.lounah.vkmc.core.recycler.base.ViewTyped
 import com.lounah.vkmc.core.recycler.paging.core.BaseViewHolder2
+import com.lounah.vkmc.core.ui.imageloader.load
+import com.lounah.vkmc.feature.feature_market.R
+import kotlinx.android.synthetic.main.item_product.*
 
 data class ProductUi(
     override val uid: String,
     val name: String,
+    val photo: String,
     val price: String,
-    override val viewType: Int = 0
+    override val viewType: Int = R.layout.item_product
 ) : ViewTyped
 
 class ProductViewHolder(
@@ -18,6 +23,8 @@ class ProductViewHolder(
 
     override fun bind(item: ProductUi) {
         super.bind(item)
-
+        image.load(item.photo, RequestOptions().encodeQuality(80))
+        name.text = item.name
+        price.text = item.price
     }
 }
