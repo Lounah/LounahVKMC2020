93dc1bfce8c2fffb9d24ff0ca56c92012e89ef84
m.shchepalin <m.shchepalin@tinkoff.ru>
2020-02-23 13:38:09 +0300

    feature/maps: setup

---
+++ feature/feature-map/src/main/java/com/lounah/vkmc/feature/feature_map/EventsMapActivity.kt
@@ -0,0 +1,12 @@
+package com.lounah.vkmc.feature.feature_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventsMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events_map)
+    }
+}
